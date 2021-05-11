8ed44f4870e09ae184f3b1a59259893b67462cb1
looleeluu <ilya.sinegubkin@gmail.com>
2021-05-11 16:53:54 +0300

    add the entry point to the app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import app from './init.js';
 
-const makeField = () => {
-  const container = document.createElement('div');
-  container.classList.add('container');
-
-  const heading = document.createElement('h1');
-  heading.classList.add('display-1');
-  heading.textContent = 'Hello Bootstrap';
-  container.append(heading);
-
-  return container;
-};
-
-document.body.appendChild(makeField());
+app();
